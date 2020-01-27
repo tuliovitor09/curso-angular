cdb65160e674348d849da44d1aa7cd3e6ad01ee3
tulio.sousa <tulio.sousa@totvs.com.br>
2020-01-27 14:31:27 -0300

    Criação de componentes e modulos

---
+++ primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { CursosModule } from './cursos/cursos.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponent
+  ],
+  imports: [
+    BrowserModule,
+    CursosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
